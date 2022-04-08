c6b27cd2e703f453ce2cbcab060d286502151c38
grahamlenert <glenert41@charlottesvilleschools.org>
2022-04-08 14:09:40 -0400

    op mode list should be clear

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RotatePIDtesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RotatePIDtesting.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -10,7 +11,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
-//@Disabled
+@Disabled
 @TeleOp(name="RotatePIDtesting", group="Linear Opmode")
 public class RotatePIDtesting extends LinearOpMode {
 
